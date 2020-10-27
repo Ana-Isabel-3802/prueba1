a41cf1705dd2574048c022a0f7600f2d219c8e11
Ana LLamas <depositodespam3802@gmail.com>
2020-10-27 13:45:22 -0600

    si quieres juega jeje

--- holamundo.py
+++ holamundo.py
@@ -1,1 +1,22 @@
+import random
 print "Hello word 3.0"
+def juego():
+    num=int(input("En que numero pienso: "))
+    a= random.randint(1, 20)
+    a=int(a)
+    acierto=False
+    for resp in range (1, 6):
+        if a < num:
+            print "El numero es menor"
+            num= int(input("Dime el numero en el que pienso: "))
+        elif a> num:
+            print "El numero es mayor"
+            num= int(input("Dime el numero en el que pienso: "))
+        elif a== num:
+            
+            resp = 6
+            acierto=True
+    if acierto==False:
+        print "Ya no tienes intentos, has perdodo el juego :("
+    else:
+        print "Felicidades acertaste :D"
--- pruebapy.py
+++ pruebapy.py
@@ -1,12 +1,16 @@
-print "Hello cold word"
-cadena=raw_input("escribe algo para acabar ahora si el programa: ")
-entero=int(input("Escrbe un entero: "))
-decimal=float(input("Escrbe un decimal: "))
-print cadena
-print entero
-print decimal
+import holamundo as h
+#print "Hello cold word"
+#cadena=raw_input("escribe algo para acabar ahora si el programa: ")
+#entero=int(input("Escrbe un entero: "))
+#decimal=float(input("Escrbe un decimal: "))
+#print cadena
+#print entero
+#print decimal
 
-if entero <= 10:
-    print "este numero solo tiene un digito"
-else:
-    print "este numero tiene mas de un digito"
+#if entero <= 10:
+ #   print "este numero solo tiene un digito"
+#else:
+#    print "este numero tiene mas de un digito"
+print "Pienso en un numero del 1 al 20"
+print "Tienes 5 intentos"
+h.juego()

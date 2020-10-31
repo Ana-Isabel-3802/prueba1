5a67a42efd57214481a90b70a1ae7ed80722948b
Alvarinix <alvaromissael1@gmail.com>
2020-10-30 23:23:06 -0600

    agregada tu sorpresa en el menu principal

--- pruebapy.py
+++ pruebapy.py
@@ -1,4 +1,5 @@
 import holamundo as h
+import calculadoraLol as lol
 #print "Hello cold word"
 #cadena=raw_input("escribe algo para acabar ahora si el programa: ")
 #entero=int(input("Escrbe un entero: "))
@@ -11,6 +12,15 @@ import holamundo as h
  #   print "este numero solo tiene un digito"
 #else:
 #    print "este numero tiene mas de un digito"
-print "Pienso en un numero del 1 al 20"
-print "Tienes 5 intentos"
-h.juego()
+
+#print "Pienso en un numero del 1 al 20"
+#print "Tienes 5 intentos"
+print "seleccione la aplicacion: "
+print "1) adivina el numero"
+print "2) calculadora de League of legends\n"
+print "cualquier otro numero) salir"
+applicacionSeleccionada=int(input("su seleccion: "))
+if (applicacionSeleccionada==1):
+    h.juego()
+elif(applicacionSeleccionada==2):
+    lol.menuCalculadora()
